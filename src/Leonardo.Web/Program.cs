950117b905e7643e3845ace2006d9a9699b0e54a
Setsuma <plug.veg@gmail.com>
2024-12-03 14:47:19 +0100

    feat: add docker yml

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,11 +1,20 @@
+using System.Text.Json.Serialization;
 using Leonardo;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Thomas!");
+app.MapGet("/", () => "Hello Setsumafu!");
 
-app.MapGet("/Fibonacci", 
-    async () => await Fibonacci.RunAsync(new []{"42", "22"}));
+app.MapGet("/fibonacci", async () =>
+{
+    var result = await Fibonacci.RunAsync(["42"]);
+    return Results.Json(result, FibonacciContext.Default.ListFibonacciResult);
+});
 
 app.Run();
+
+[JsonSerializable(typeof(List<FibonacciResult>))]
+internal partial class FibonacciContext : JsonSerializerContext
+{
+}
