d7e9721568809f06e8bfc9597364c38607ec83d7
Setsuma <plug.veg@gmail.com>
2024-12-17 14:57:31 +0100

    feat: add appsettings file & show it on run demo

--- src/Demo1/Program.cs
+++ src/Demo1/Program.cs
@@ -1,6 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 using Leonardo;
+using Microsoft.Extensions.Configuration;
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");   
+IConfiguration configuration = new ConfigurationBuilder() 
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddEnvironmentVariables()        
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)   
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true) 
+    .Build();
+var applicationName = configuration.GetValue<string>("Application:Name");  
+var applicationMessage = configuration.GetValue<string>("Application:Message");   
+Console.WriteLine($"Application Name : {applicationName}");   
+Console.WriteLine($"Application Message : {applicationMessage}"); 
 
 var stopwatch = new Stopwatch();
 stopwatch.Start();
