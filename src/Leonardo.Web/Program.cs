9fbd499a7b8fb1a944e0511c6a57163e0d68ce89
Setsuma <plug.veg@gmail.com>
2024-12-17 16:27:55 +0100

    fix: update webapp Leonardo.Web

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,16 +1,21 @@
 using System.Text.Json.Serialization;
 using Leonardo;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<FibonacciDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<Fibonacci>();
+
 var app = builder.Build();
 
 await using FibonacciDataContext context = new();
 
 app.MapGet("/", () => "Hello Setsumafu!");
 
-app.MapGet("/fibonacci", async () =>
+app.MapGet("/fibonacci", async (Fibonacci fibonacci, ILogger<Program> logger) =>
 {
-    var result = await new Fibonacci(context).RunAsync(["42"]);
+    logger.LogInformation("Requesting Fibonacci");
+    var result = await fibonacci.RunAsync(["42"]);
     return Results.Json(result, FibonacciContext.Default.ListFibonacciResult);
 });
 
