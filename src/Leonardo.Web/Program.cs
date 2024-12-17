9b73765350594d803363097777f9725968821ecb
Setsuma <plug.veg@gmail.com>
2024-12-17 14:18:04 +0100

    fix: update Leonardo.Web

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -4,11 +4,13 @@ using Leonardo;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+await using FibonacciDataContext context = new();
+
 app.MapGet("/", () => "Hello Setsumafu!");
 
 app.MapGet("/fibonacci", async () =>
 {
-    var result = await Fibonacci.RunAsync(["42"]);
+    var result = await new Fibonacci(context).RunAsync(["42"]);
     return Results.Json(result, FibonacciContext.Default.ListFibonacciResult);
 });
 
